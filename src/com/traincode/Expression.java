2a45eeafa96eadec75528d4ef6d934ad82a5e6d9
Dan <dan@dan.com>
2020-08-09 14:44:16 +0300

    Stack: Implement check of balanced strings.

--- src/com/traincode/Expression.java
+++ src/com/traincode/Expression.java
@@ -1,20 +1,38 @@
 package com.traincode;
 
-import java.util.Stack;
+import java.util.*;
 
 public class Expression {
+
+    private final List<Character> leftBrackets = Arrays.asList('(', '<', '[', '{');
+    private final List<Character> rightBrackets = Arrays.asList(')', '>', ']', '}');
+
     public boolean isBalanced(String input) {
         Stack<Character> stack = new Stack<>();
 
         for (char ch : input.toCharArray()) {
-            if (ch == '(')
+            if (isLeftBracket(ch))
                 stack.push(ch);
-            if (ch == ')') {
+            if (isRightBracket(ch)) {
                 if (stack.empty()) return false;
-                stack.pop();
+                
+                char top = stack.pop();
+                if (!bracketsMatch(top, ch)) return false;
             }
         }
 
         return stack.empty();
+    }
+
+    private boolean isLeftBracket(char ch) {
+        return leftBrackets.contains(ch);
+    }
+
+    private boolean isRightBracket(char ch) {
+        return rightBrackets.contains(ch);
+    }
+
+    private boolean bracketsMatch(char left, char right) {
+        return leftBrackets.indexOf(left) == rightBrackets.indexOf(right);
     }
 }
--- src/com/traincode/Main.java
+++ src/com/traincode/Main.java
@@ -8,6 +8,8 @@ public class Main {
         // Edge cases
         // (
         // (()
+        // ( >
+        // ( ]
         // ) (
         String str0 = "(1 + 2)"; // balanced string
         String str1 = "(1 + 2"; // not balanced string
@@ -18,10 +20,13 @@ public class Main {
         String edge0 = "(1 + 2";
         String edge1 = "((1 + 2)";
         String edge2 = ")1 + 2(";
+        String edge3 = "(1 + 2>";
+        String edge4 = "(1 + 2]";
+        String edge5 = "(1 + 2}";
 
         // Let's work only with str0
         Expression exp = new Expression();
-        Boolean result = exp.isBalanced(edge2);
+        Boolean result = exp.isBalanced(edge5);
         System.out.println(result);
 
     }
