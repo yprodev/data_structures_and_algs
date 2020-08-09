d6c221b4b185ac45ea7dfeeee0be5ec962e79b83
Dan <dan@dan.com>
2020-08-09 13:00:27 +0300

    Linked Lists: Implement method to find Kth node from the end of the linked list.

--- src/com/traincode/LinkedList.java
+++ src/com/traincode/LinkedList.java
@@ -142,6 +142,29 @@ public class LinkedList {
 
     }
 
+    public int getKthFromTheEnd(int k) {
+        if (isEmpty())
+            throw new IllegalStateException();
+
+        Node a = first;
+        Node b = first;
+
+        for (int i = 0; i < k - 1; i++) {
+            b = b.next;
+            if (b == null)
+                throw new IllegalArgumentException();
+        }
+
+        while (b != last) {
+            a = a.next;
+            b = b.next;
+        }
+
+        return a.value;
+
+
+    }
+
     private boolean isEmpty() {
         return first == null;
     }
--- src/com/traincode/Main.java
+++ src/com/traincode/Main.java
@@ -13,9 +13,10 @@ public class Main {
         list.addLast(20);
         list.addLast(30);
         list.addLast(40);
-        list.reverse();
-        int[] array = list.toArray();
-        System.out.println(Arrays.toString(array));
+        list.addLast(50);
+        list.addLast(60);
+        list.addLast(70);
+        System.out.println(list.getKthFromTheEnd(2));
 
     }
 }
