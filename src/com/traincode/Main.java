e67a8e0e206e8910f535e6c53ce5164a1434a0e7
Dan <dan@dan.com>
2020-08-15 16:20:43 +0300

    Queue: Implement own Queue data structure.

--- src/com/traincode/Main.java
+++ src/com/traincode/Main.java
@@ -8,23 +8,13 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-        Queue<Integer> queue = new ArrayDeque<>();
+        ArrayQueue queue = new ArrayQueue(5);
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        int front = queue.dequeue();
 
-        queue.add(10);
-        queue.add(20);
-        queue.add(30);
-
-        reverse(queue);
-
+        System.out.println(front);
         System.out.println(queue);
-
-    }
-
-    public static void reverse(Queue<Integer> queue) {
-        Stack<Integer> stack = new Stack<>();
-        while (!queue.isEmpty())
-            stack.push(queue.remove());
-        while (!stack.isEmpty())
-            queue.add(stack.pop());
     }
 }
