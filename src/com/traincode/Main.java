e5d23e2fd9327ac1d33f6950f3bfcd3e9b7f5328
Dan <dan@dan.com>
2020-08-16 15:32:56 +0300

    HashTable: Build-in HashTable in Java - HashMap.

--- src/com/traincode/Main.java
+++ src/com/traincode/Main.java
@@ -1,20 +1,31 @@
 package com.traincode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        PriorityQueue queue = new PriorityQueue();
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "Steven");
+        map.put(2, "John");
+        map.put(3, "Elizabeth");
+        map.put(3, "Alice");
+        map.put(4, null);
+        map.put(null, null);
+        map.remove(null);
+        String alice = map.get(3);
+        boolean isThreeKey = map.containsKey(3); // O(1)
+        boolean isJohn = map.containsValue("John"); // O(n)
 
-        queue.add(5);
-        queue.add(3);
-        queue.add(6);
-        queue.add(1);
-        queue.add(4);
+        System.out.println(map);
+        System.out.println("is 3 key? " + isThreeKey);
+        System.out.println("is John value? " + isJohn);
+        System.out.println(alice);
 
-        System.out.println(queue);
+        for (int item : map.keySet())
+            System.out.println(item);
 
-        while(!queue.isEmpty())
-            System.out.println(queue.remove());
-
-
+        for (Map.Entry<Integer, String> item : map.entrySet())
+            System.out.println(item);
     }
 }
