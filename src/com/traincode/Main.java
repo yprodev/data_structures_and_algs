08490828d304c48564ab2c7548835f83fdbed672
Dan <dan@dan.com>
2020-08-16 13:54:35 +0300

    Queue: Impelement queue with two stacks.

--- src/com/traincode/Main.java
+++ src/com/traincode/Main.java
@@ -1,24 +1,15 @@
 package com.traincode;
 
-import java.util.ArrayDeque;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Queue;
-import java.util.Stack;
-
 public class Main {
     public static void main(String[] args) {
-        ArrayQueue queue = new ArrayQueue(5);
+        QueueWithTwoStacks queue = new QueueWithTwoStacks();
         queue.enqueue(10);
         queue.enqueue(20);
         queue.enqueue(30);
-        int front = queue.dequeue();
+        int first = queue.dequeue();
+        int second = queue.peek();
 
-        queue.enqueue(40);
-        queue.enqueue(50);
-        queue.enqueue(60);
-//        queue.enqueue(70);
-        System.out.println(front);
-        System.out.println(queue);
+        System.out.println(first);
+        System.out.println(second);
     }
 }
