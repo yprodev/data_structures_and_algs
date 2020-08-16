03fe726bc57edb613c77dff23e639056eb4eec69
Dan <dan@dan.com>
2020-08-16 20:04:05 +0300

    HashTable: The simplest hash function is a modulo operation. Built-in string hash method.

--- src/com/traincode/Main.java
+++ src/com/traincode/Main.java
@@ -1,9 +1,24 @@
 package com.traincode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        CharFinder finder = new CharFinder();
-        char ch = finder.findFirstRepeatedChar("green apple");
-        System.out.println(ch);
+        // built-in hash function
+        String s = "watermelon";
+        System.out.println(s.hashCode());
+
+        Map<String, String> map = new HashMap<>();
+        map.put("123456-A", "John");
+        System.out.println(hash("123456-A"));
+    }
+
+    public static int hash(String key) {
+        int hash = 0;
+        for (char ch : key.toCharArray())
+            hash += ch;
+
+        return hash % 100;
     }
 }
