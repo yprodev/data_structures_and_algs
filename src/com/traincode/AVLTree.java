5a6cf14d22dc9f59463e070b226c329352f938a8
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2020-08-24 12:59:33 +0300

    AVL Tree: Implement height method.

--- src/com/traincode/AVLTree.java
+++ src/com/traincode/AVLTree.java
@@ -3,6 +3,7 @@ package com.traincode;
 public class AVLTree {
     private class AVLNode {
         private int value;
+        private int height;
         private AVLNode leftChild;
         private AVLNode rightChild;
 
@@ -31,6 +32,14 @@ public class AVLTree {
         else
             root.rightChild = insert(root.rightChild, value);
 
+        root.height = Math.max(
+                height(root.leftChild),
+                height(root.rightChild)) + 1;
+
         return root;
+    }
+
+    private int height(AVLNode node) {
+        return (node == null) ? -1 : node.height;
     }
 }
