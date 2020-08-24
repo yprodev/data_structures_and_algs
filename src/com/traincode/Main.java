d94a54e520c0347d17ce3e614a509bbd96fdd66c
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2020-08-24 12:53:14 +0300

    AVL Tree: Implement self-balancing AVL (Adelson-Velsky and Landis) tree. Impelement recursion based insert method.

--- src/com/traincode/Main.java
+++ src/com/traincode/Main.java
@@ -1,19 +1,12 @@
 package com.traincode;
 
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        Tree tree = new Tree();
+        AVLTree tree = new AVLTree();
 
-        tree.insert(7);
-        tree.insert(4);
-        tree.insert(9);
-        tree.insert(1);
-        tree.insert(6);
-        tree.insert(8);
         tree.insert(10);
+        tree.insert(20);
+        tree.insert(30);
 
-        tree.traverseLevelOrder();
     }
 }
