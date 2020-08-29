21acef9bd21fe8200438f2e4e8d78b31ca9a7be3
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2020-08-29 15:46:33 +0300

    Heap: Priority queue with using Heap data structure.

--- src/com/traincode/Main.java
+++ src/com/traincode/Main.java
@@ -1,26 +1,15 @@
 package com.traincode;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = { 5, 3, 10, 1, 4, 2};
-
-        Heap heap = new Heap();
-        for (int number : numbers)
-            heap.insert(number);
-
-//        while(!heap.isEmpty())
-//            System.out.println(heap.remove());
-
-        // DESC order
-//        for (int i = 0; i < numbers.length; i++)
-//            numbers[i] = heap.remove();
-
-        // ASC order
-        for (int i = numbers.length - 1; i >= 0; i--)
-            numbers[i] = heap.remove();
-
-        System.out.println(Arrays.toString(numbers));
+        // operation:   Priority Queue       Heap
+        // insert:      O(n)                 O(log n)
+        // remove:      O(1)                 O(log n)
+        PriorityQueue queue = new PriorityQueue();
+        queue.add(30);
+        queue.add(20);
+        queue.add(10);
+        queue.add(5);
+        System.out.println(queue);
     }
 }
