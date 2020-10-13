0319007963163a9123569d73cfb84f3ab2910f38
Yaroslav Produnchuk <yprodanchuk@gmail.com>
2020-10-13 14:26:19 +0300

    [Graph-Undirected]: Implement undirected graph.

--- src/com/traincode/Main.java
+++ src/com/traincode/Main.java
@@ -5,17 +5,16 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        var graph = new Graph();
+        var graph = new WeightedGraph();
+
         graph.addNode("A");
         graph.addNode("B");
         graph.addNode("C");
 
-        graph.addEdge("A", "B");
-        graph.addEdge("B", "C");
-        graph.addEdge("C", "A");
+        graph.addEdge("A", "B", 3);
+        graph.addEdge("A", "C", 2);
 
-
-        System.out.println(graph.hasCycle());
+        graph.print();
     }
 
 }
