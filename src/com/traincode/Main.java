83233ea9fdcd8482686919bc3b3a15c7b4c1c465
Yaroslav Produnchuk <yprodanchuk@gmail.com>
2020-10-15 18:10:39 +0300

    [Graph-Undirected]: Imlement Prims algorithm (minimal spanning tree).

--- src/com/traincode/Main.java
+++ src/com/traincode/Main.java
@@ -10,12 +10,18 @@ public class Main {
         graph.addNode("A");
         graph.addNode("B");
         graph.addNode("C");
+        graph.addNode("D");
+
 
         graph.addEdge("A", "B", 0);
+        graph.addEdge("B", "D", 0);
+        graph.addEdge("C", "D", 0);
         graph.addEdge("B", "C", 2);
         graph.addEdge("A", "C", 10);
 
-        System.out.println(graph.hasCycle());
+        var tree = graph.getMinimumSpanningTree();
+
+        tree.print();
     }
 
 }
