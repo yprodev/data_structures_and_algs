aca05dec942d340ec144e08a5e7deefcd733d94e
Yaroslav Produnchuk <yprodanchuk@gmail.com>
2020-10-18 09:23:49 +0300

    [String]: Remove duplicated chars in a string.

--- src/com/traincode/Main.java
+++ src/com/traincode/Main.java
@@ -6,8 +6,8 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Boolean rotation = StringUtils.areRotations("ABCD", "BCDA");
+        String uniqueStr = StringUtils.removeDuplicates("Trees are beautiful");
 
-        System.out.println(rotation);
+        System.out.println(uniqueStr);
     }
 }
--- src/com/traincode/StringUtils.java
+++ src/com/traincode/StringUtils.java
@@ -2,6 +2,8 @@ package com.traincode;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class StringUtils {
     // Number of vowels
@@ -54,5 +56,22 @@ public class StringUtils {
             return false;
 
         return str1.length() == str2.length() && (str1 + str1).contains(str2);
+    }
+
+    public static String removeDuplicates(String str) {
+        if (str == null)
+            return "";
+
+        StringBuilder output = new StringBuilder();
+        Set<Character> seen = new HashSet<>();
+
+        for (var ch : str.toCharArray()) {
+            if (!seen.contains(ch)) {
+                seen.add(ch);
+                output.append(ch);
+            }
+        }
+
+        return output.toString();
     }
 }
